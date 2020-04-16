e86c398b14f7d34bbc7e7fe6a4a5784f8da75cfd
Grigory Sharov <sharov.grigory@gmail.com>
2020-04-16 13:45:11 +0100

    3.0.0a1 release

--- setup.py
+++ setup.py
@@ -65,7 +65,7 @@ setup(
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.1.0',  # Required
+    version='3.0.0a1',  # Required
 
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
@@ -105,7 +105,7 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 4 - Beta',
+        'Development Status :: 3 - Alpha',
 
         # Indicate who your project is intended for
         #   'Intended Audience :: Users',
