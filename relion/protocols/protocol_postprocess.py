d70c01d87f6c840f6bfd2d49ac31046b7bc21602
Grigory Sharov <sharov.grigory@gmail.com>
2020-04-29 19:16:09 +0100

    we cant distribute those :(

--- relion/protocols/protocol_postprocess.py
+++ relion/protocols/protocol_postprocess.py
@@ -97,7 +97,7 @@ class ProtRelionPostprocess(ProtAnalysis3D):
                             '- [[https://www3.mrc-lmb.cam.ac.uk/relion/index.php/'
                             'FAQs#Where_can_I_find_MTF_curves_for_typical_detectors.3F]'
                             '[Relion\'s Wiki FAQs]]\n'
-                            ' - [[http://www.gatan.com/K3][Gatan\'s website]]\n\n'
+                            ' - [[https://www.gatan.com/techniques/cryo-em#MTF][Gatan\'s website]]\n\n'
                             'Relion param: *--mtf*')
         if Plugin.IS_GT30():
             group.addParam('origPixelSize', params.FloatParam,
