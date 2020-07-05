9fa472b86a26b8bbe6d9262084afeeaba19f3758
J.M. de la Rosa Trevin <delarosatrevin@gmail.com>
2020-07-05 18:00:04 +0200

    Updated assign optics protocol

--- relion/protocols/protocol_assign_optic_groups.py
+++ relion/protocols/protocol_assign_optic_groups.py
@@ -25,6 +25,9 @@
 # **************************************************************************
 import os
 
+import emtable
+import pyworkflow as pw
+import pyworkflow.utils as pwutils
 import pwem
 import pyworkflow.protocol.params as params
 
@@ -47,54 +50,77 @@ class ProtRelionAssignOpticsGroup(ProtRelionBase):
                       help='Select the input set (Movies, Micrographs or '
                            'Particles) to assign Optics Group parameters.')
 
-        form.addParam('opticsGroupName', params.StringParam,
-                      default='opticsGroup1',
-                      label='Optics group name',
-                      help='Relion-specific option. Name of this optics group. '
-                           'Each group of movies with different '
-                           'optics characteristics for CTF refinement '
-                           'should have a unique name.')
+        form.addParam('inputType', params.EnumParam, default=0,
+                      choices=['single group params', 'groups from star'],
+                      display=params.EnumParam.DISPLAY_LIST,
+                      label='Input Optics Groups from',
+                      help='Select how to provide information about the optics '
+                           'groups. In the case of *single ')
 
-        form.addParam('mtfFile', params.FileParam, allowsNull=True,
-                      label='MTF-curve file',
-                      help='User-provided STAR-file with the MTF-curve '
-                           'of the detector. Use the wizard to load one '
-                           'of the predefined ones provided at:\n'
-                           '- [[https://www3.mrc-lmb.cam.ac.uk/relion/index.php/'
-                           'FAQs#Where_can_I_find_MTF_curves_for_typical_detectors.3F]'
-                           '[Relion\'s Wiki FAQs]]\n'
-                           ' - [[https://www.gatan.com/techniques/cryo-em#MTF][Gatan\'s website]]\n\n'
-                           'Relion param: *--mtf*')
+        group = form.addGroup("Optics Group params",
+                              condition="inputType == 0")
 
-        line = form.addLine('Beam tilt (mrad)',
-                            help='Known beam tilt in the X/Y-direction (in mrad). '
-                                 'Set to zero if unknown.')
+        group.addParam('opticsGroupName', params.StringParam,
+                       default='opticsGroup1',
+                       label='Optics group name',
+                       help='Relion-specific option. Name of this optics group. '
+                            'Each group of movies with different '
+                            'optics characteristics for CTF refinement '
+                            'should have a unique name.')
+
+        group.addParam('mtfFile', params.FileParam, allowsNull=True,
+                       label='MTF-curve file',
+                       help='User-provided STAR-file with the MTF-curve '
+                            'of the detector. Use the wizard to load one '
+                            'of the predefined ones provided at:\n'
+                            '- [[https://www3.mrc-lmb.cam.ac.uk/relion/index.php/'
+                            'FAQs#Where_can_I_find_MTF_curves_for_typical_detectors.3F]'
+                            '[Relion\'s Wiki FAQs]]\n'
+                            ' - [[https://www.gatan.com/techniques/cryo-em#MTF][Gatan\'s website]]\n\n'
+                            'Relion param: *--mtf*')
+
+        line = group.addLine('Beam tilt (mrad)',
+                             help='Known beam tilt in the X/Y-direction (in mrad). '
+                                  'Set to zero if unknown.')
         line.addParam('beamTiltX', params.FloatParam, default=0.,
                       label='X')
         line.addParam('beamTiltY', params.FloatParam, default=0.,
                       label='Y')
 
-        form.addParam('defectFile', params.FileParam, allowsNull=True,
-                      label='Defects file',
-                      help='Location of a UCSF MotionCor2-style '
-                      'defect text file or a defect map that '
-                      'describe the defect pixels on the detector. '
-                      'Each line of a defect text file should contain '
-                      'four numbers specifying x, y, width and height '
-                      'of a defect region. A defect map is an image '
-                      '(MRC or TIFF), where 0 means good and 1 means '
-                      'bad pixels. The coordinate system is the same '
-                      'as the input movie before application of '
-                      'binning, rotation and/or flipping.\n\n'
-                      '_Note that the format of the defect text is '
-                      'DIFFERENT from the defect text produced '
-                      'by SerialEM!_\n One can convert a SerialEM-style '
-                      'defect file into a defect map using IMOD '
-                      'utilities e.g.:\n'
-                      '*clip defect -D defect.txt -f tif movie.tif defect_map.tif*\n'
-                      'See explanations in the SerialEM manual.\n'
-                      'Leave empty if you do not have any defects, '
-                      'or do not want to correct for defects on your detector.')
+        group.addParam('defectFile', params.FileParam, allowsNull=True,
+                       label='Defects file',
+                       help='Location of a UCSF MotionCor2-style '
+                       'defect text file or a defect map that '
+                       'describe the defect pixels on the detector. '
+                       'Each line of a defect text file should contain '
+                       'four numbers specifying x, y, width and height '
+                       'of a defect region. A defect map is an image '
+                       '(MRC or TIFF), where 0 means good and 1 means '
+                       'bad pixels. The coordinate system is the same '
+                       'as the input movie before application of '
+                       'binning, rotation and/or flipping.\n\n'
+                       '_Note that the format of the defect text is '
+                       'DIFFERENT from the defect text produced '
+                       'by SerialEM!_\n One can convert a SerialEM-style '
+                       'defect file into a defect map using IMOD '
+                       'utilities e.g.:\n'
+                       '*clip defect -D defect.txt -f tif movie.tif defect_map.tif*\n'
+                       'See explanations in the SerialEM manual.\n'
+                       'Leave empty if you do not have any defects, '
+                       'or do not want to correct for defects on your detector.')
+
+        form.addParam('inputStar', params.FileParam,
+                      condition='inputType == 1',
+                      label='Input Star file',
+                      help='Provide input star file with Optics groups '
+                           'information. The input Star file should contains: \n'
+                           '- *optics* table with values for each group.\n'
+                           '- *micrographs* Table with two columns: \n'
+                           '   * rlnMicrographName with the micName associated to'
+                           '   the input set.'
+                           '   * rlnOpticsGroup with the group associted to this'
+                           '   micrograph.'
+                      )
 
     # --------------------------- INSERT steps functions ----------------------
     def _insertAllSteps(self):
@@ -104,7 +130,9 @@ class ProtRelionAssignOpticsGroup(ProtRelionBase):
     # --------------------------- STEPS functions -----------------------------
     def createOutputStep(self, inputId):
         inputSet = self.inputSet.get()
-        
+
+        getMicName = lambda item: item.getMicName()
+
         if isinstance(inputSet, pwem.objects.SetOfMovies):
             outputSet = self._createSetOfMovies()
             outputName = 'outputMovies'
@@ -114,6 +142,7 @@ class ProtRelionAssignOpticsGroup(ProtRelionBase):
         elif isinstance(inputSet, pwem.objects.SetOfParticles):
             outputSet = self._createSetOfParticles()
             outputName = 'outputParticles'
+            getMicName = lambda item: item.getCoordinate().getMicName()
         else:
             raise Exception("Invalid input of type %s, expecting:\n"
                             "SetOfMovies, SetOfMicrographs or SetOfParticles"
@@ -121,25 +150,65 @@ class ProtRelionAssignOpticsGroup(ProtRelionBase):
 
         # Copy general info from input set
         outputSet.copyInfo(inputSet)
-        # Update the acquisition object with new parameters from input
-        acq = inputSet.getAcquisition()
-        outputSet.copyItems(inputSet)
 
-        og = OpticsGroups.create(
-            rlnVoltage=acq.getVoltage(),
-            rlnSphericalAberration=acq.getSphericalAberration(),
-            rlnAmplitudeContrast=acq.getAmplitudeContrast(),
-            rlnImagePixelSize=inputSet.getSamplingRate(),
-            rlnImageSize=inputSet.getXDim(),
-            rlnOpticsGroupName=self.opticsGroupName.get(),
-            rlnBeamTiltX=self.beamTiltX.get(),
-            rlnBeamTiltY=self.beamTiltY.get()
-        )
 
-        if self.mtfFile.hasValue():
-            og.addColumns(rlnMtfFileName=self.mtfFile.get())
-        if self.defectFile.hasValue():
-            og.addColumns(rlnMicrographDefectFile=self.defectFile.get())
+        if self.inputType == 0:  # single group params
+            og = OpticsGroups.fromImages(inputSet)
+            outputSet.copyItems(inputSet)
+
+            if len(og) > 1:
+                raise Exception("Assigning single optics groups params "
+                                "is valid only when the input contains at "
+                                "most one optics groups. ")
+
+            acq = inputSet.getAcquisition()
+            og = OpticsGroups.create(
+                rlnVoltage=acq.getVoltage(),
+                rlnSphericalAberration=acq.getSphericalAberration(),
+                rlnAmplitudeContrast=acq.getAmplitudeContrast(),
+                rlnImagePixelSize=inputSet.getSamplingRate(),
+                rlnImageSize=inputSet.getXDim(),
+                rlnOpticsGroupName=self.opticsGroupName.get(),
+                rlnBeamTiltX=self.beamTiltX.get(),
+                rlnBeamTiltY=self.beamTiltY.get()
+            )
+
+            if self.mtfFile.hasValue():
+                inputMtf = self.mtfFile.get()
+                outputMtf = self._getPath(os.path.basename(inputMtf))
+                pwutils.copyFile(inputMtf, outputMtf)
+                og.addColumns(rlnMtfFileName=outputMtf)
+            if self.defectFile.hasValue():
+                og.addColumns(rlnMicrographDefectFile=self.defectFile.get())
+        else:
+            inputStar = self.inputStar.get()
+            og = OpticsGroups.fromStar(inputStar)
+            micTable = emtable.Table(fileName=inputStar,
+                                     tableName='micrographs')
+            micDict = {row.rlnMicrographName: row.rlnOpticsGroup
+                       for row in micTable}
+
+            def updateItem(item, row):
+                micName = getMicName(item)
+
+                if not micName in micDict:
+                    raise Exception("Micrograph name (aka micName) '%s' was "
+                                    "not found on the 'micrographs' table of "
+                                    "the input star file: %s"
+                                    % (micName, inputStar))
+
+                ogNumber = micDict[micName]
+
+                if not hasattr(item, '_rlnOpticsGroup'):
+                    item._rlnOpticsGroup = pw.object.Float()
+
+                item._rlnOpticsGroup.set(ogNumber)
+
+            outputSet.copyItems(inputSet,
+                                updateItemCallback=updateItem,
+                                doClone=False)
+
+        print(og)
 
         og.toImages(outputSet)
 
