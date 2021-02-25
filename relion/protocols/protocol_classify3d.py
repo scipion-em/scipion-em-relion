6d451925f2b37282f9148a8fc6ac83dd4e56d511
Your Name <you@example.com>
2021-02-25 09:14:11 +0100

    check if sigma_ang is in extra params before adding the default value

--- relion/protocols/protocol_classify3d.py
+++ relion/protocols/protocol_classify3d.py
@@ -72,8 +72,10 @@ class ProtRelionClassify3D(ProtClassify3D, ProtRelionBase):
             # before adding the default value
             if self.localAngularSearch:
                 if self.extraParams.hasValue():
-                    if self.extraParams.get().find("--sigma_ang") == -1:
+                    if self.extraParams.get().find("--sigma_ang") != -1:
                          args['--sigma_ang'] = self.localAngularSearchRange.get() / 3.
+                else:
+                    args['--sigma_ang'] = self.localAngularSearchRange.get() / 3.
 
             if relion.Plugin.IS_GT30() and self.allowCoarserSampling:
                 args['--allow_coarser_sampling'] = ''
