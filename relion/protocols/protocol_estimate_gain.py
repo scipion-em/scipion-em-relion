ea0adb738b4bf62243e474793b5278501275ee87
Grigory Sharov <sharov.grigory@gmail.com>
2024-05-18 14:13:12 +0100

    gain estimation refactored, test added

--- relion/protocols/protocol_estimate_gain.py
+++ relion/protocols/protocol_estimate_gain.py
@@ -1,8 +1,10 @@
 # ******************************************************************************
 # *
 # * Authors:     J.M. De la Rosa Trevin (delarosatrevin@scilifelab.se) [1]
+# * Authors:     Grigory Sharov (gsharov@mrc-lmb.cam.ac.uk) [2]
 # *
 # * [1] SciLifeLab, Stockholm University
+# * [2] MRC Laboratory of Molecular Biology, MRC-LMB
 # *
 # * This program is free software; you can redistribute it and/or modify
 # * it under the terms of the GNU General Public License as published by
@@ -26,28 +28,32 @@
 
 import pyworkflow.protocol.params as params
 import pyworkflow.utils as pwutils
-from pyworkflow.protocol import STEPS_SERIAL
+from pyworkflow.protocol.constants import STEPS_SERIAL
 from pyworkflow.constants import PROD
 from pwem.protocols import ProtProcessMovies
+import pwem.emlib as emlib
 
 import relion.convert as convert
 
 
 class ProtRelionCompressEstimateGain(ProtProcessMovies):
     """
-    Using *relion_convert_to_tiff* to estimate the gain reference from a set of movies.
+    Using *relion_estimate_gain* or *relion_compress_to_tiff* to estimate the gain reference from a set of movies.
     """
     _label = 'estimate gain reference'
     _devStatus = PROD
 
     def __init__(self, **kwargs):
         ProtProcessMovies.__init__(self, **kwargs)
+        self.isFloat32 = False
+        self.isEER = False
         self.stepsExecutionMode = STEPS_SERIAL
 
     def _getConvertExtension(self, filename):
         """ Check whether it is needed to convert to .mrc or not """
         ext = pwutils.getExt(filename).lower()
-        return None if ext in ['.mrc', '.mrcs', '.tiff', '.tif'] else 'mrc'
+        return None if ext in ['.mrc', '.mrcs', '.tiff',
+                               '.tif', '.eer', '.gain'] else 'mrc'
 
     def _createFilenameTemplates(self):
         """ Centralize how files are called. """
@@ -66,10 +72,28 @@ class ProtRelionCompressEstimateGain(ProtProcessMovies):
                       label=pwutils.Message.LABEL_INPUT_MOVS,
                       help='Select a set of movies to be used in the gain '
                            'estimation.')
-        form.addParam('moviesSubset', params.IntParam, default=0,
-                      label='Subset',
-                      help="Use a subset of the movies for the estimation. "
-                           "If 0, all input movies will be used. ")
+        form.addParam('threshold', params.IntParam, default=50,
+                      expertLevel=params.LEVEL_ADVANCED,
+                      label='Pixel threshold',
+                      help="*Only for 32-bit float MRC*\nNumber of success "
+                           "needed to consider a pixel reliable. A pixel is "
+                           "considered to be reliable when values which are "
+                           "integer multiples of the current gain estimate "
+                           "were observed at least --thresh times "
+                           "(default 50) without being interrupted by mismatch.")
+        form.addParam('maxFrames', params.IntParam, default=0,
+                      label="Target number of frames to average",
+                      help="Default 0 means use all.")
+        form.addParam('random', params.BooleanParam, default=False,
+                      label="Randomise input",
+                      help="Randomise the order of input movies before "
+                           "taking subset")
+        form.addParam('eerSampling', params.EnumParam, default=1,
+                      choices=['1x', '2x'],
+                      display=params.EnumParam.DISPLAY_HLIST,
+                      label='EER upsampling',
+                      help="EER upsampling (1x = physical 4K, "
+                           "2x = super-resolution 8K)")
 
         form.addParallelSection(threads=4, mpi=0)
 
@@ -77,40 +101,52 @@ class ProtRelionCompressEstimateGain(ProtProcessMovies):
     def _insertAllSteps(self):
         self._createFilenameTemplates()
         self._insertFunctionStep('convertInputStep',
-                                 self.inputMovies.getObjId(),
-                                 self.moviesSubset.get())
-
+                                 self.inputMovies.getObjId())
         self._insertFunctionStep('estimateGainStep')
-        self._insertFunctionStep('createOutputStep')
 
-    def convertInputStep(self, moviesId, subset):
+    def convertInputStep(self, moviesId):
         self.info("Relion version:")
-        self.runJob("relion_convert_to_tiff --version", "", numberOfMpi=1)
-
-        moviesList = self.inputMovies.get()
-        if subset > 0:
-            moviesList = [m.clone()
-                          for i, m in enumerate(moviesList) if i < subset]
-
+        self.runJob("relion_estimate_gain --version", "",
+                    numberOfMpi=1)
         writer = convert.createWriter()
-        writer.writeSetOfMovies(moviesList, self._getFileName("input_star"))
+        writer.writeSetOfMovies(self.inputMovies.get(),
+                                self._getFileName("input_star"))
 
     def estimateGainStep(self):
-        args = " --i %s --o %s" % (self._getFileName("input_star"),
-                                   self._getPath())
-        args += " --estimate_gain --j %d" % self.numberOfThreads
+        args = [
+            f"--i {self._getFileName('input_star')}",
+            f"--j {self.numberOfThreads}",
+        ]
 
-        self.runJob('relion_convert_to_tiff', args)
+        if self.isFloat32:
+            program = 'relion_convert_to_tiff'
+            args.extend([
+                f"--thresh {self.threshold}",
+                f"--o {self.getPath()}",
+                "--estimate_gain"
+            ])
+        else:
+            program = 'relion_estimate_gain'
+            args.extend([
+                f"--max_frames {self.maxFrames.get() if self.maxFrames.get() > 0 else -1}",
+                f"--eer_upsampling {self.eerSampling.get()+1}",
+                f"--o {self.getPath('gain_estimate.mrc')}"
+            ])
+            if self.random:
+                args.append('--random')
+            if self.isEER:
+                args.append('--dont_invert')
 
-    def createOutputStep(self):
-        pass
+        self.runJob(program, " ".join(args))
 
     def _stepsCheck(self):
         pass
 
     # --------------------------- INFO functions ------------------------------
     def _summary(self):
-        summary = ["Using relion_convert_to_tiff to estimate gain reference"]
+        summary = []
+        if self.isFinished():
+            summary.append('Gain estimation completed')
         return summary
 
     def _citations(self):
@@ -119,13 +155,13 @@ class ProtRelionCompressEstimateGain(ProtProcessMovies):
     def _validate(self):
         errors = []
         inputMovies = self.inputMovies.get()
-
-        if self.moviesSubset.get() > len(inputMovies):
-            errors.append("Subset size cannot be bigger than input set!")
-
         firstMovie = inputMovies.getFirstItem()
-        if pwutils.getExt(firstMovie.getFileName()) not in [".mrcs", ".mrc"]:
-            errors.append("Gain estimation only makes sense for 32-bit float MRC(S) format.")
+        fn = firstMovie.getFileName()
+
+        ih = emlib.image.ImageHandler()
+        if "mrc" in pwutils.getExt(fn) and ih.getDataType(fn) == emlib.DT_FLOAT:
+            self.isFloat32 = True
+        if pwutils.getExt(fn) == ".eer":
+            self.isEER = True
 
         return errors
-
