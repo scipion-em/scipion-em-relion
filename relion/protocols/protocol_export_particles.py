173666cf98c6641c8601ac859968beebfadb7c96
James Krieger <jamesmkrieger@gmail.com>
2025-12-23 16:51:42 +0000

    useAbsPath condition for multi too

--- relion/protocols/protocol_export_particles.py
+++ relion/protocols/protocol_export_particles.py
@@ -80,7 +80,7 @@ class ProtRelionExportParticles(ProtProcessParticles, ProtRelionBase):
                            "reflected in the star file")
 
         form.addParam('useAbsPath', params.BooleanParam, default=False,
-                      condition='stackType==%d' % STACK_ONE,
+                      condition='stackType!=%d' % 0, # Only if stacks are written
                       label='Use absolute path?',
                       help='If *Yes* the absolute path will be used in the '
                            'star file. This is only used for single stack export.')
